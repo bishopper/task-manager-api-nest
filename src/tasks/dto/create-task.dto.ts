44cfea4d04488f464e733f1424dd47c20630346c
Mohammad Behnam <mohammadbehnam.dev@gmail.com>
2024-09-22 12:28:57 +0330

    Update create-task.dto.ts

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -3,6 +3,6 @@ import { TaskStatus } from '../entities/task.entity';
 export class CreateTaskDto {
   title: string;
   description?: string;
-  status: TaskStatus; // استفاده از TaskStatus به جای string
+  status: TaskStatus;
   projectId: number;
 }
