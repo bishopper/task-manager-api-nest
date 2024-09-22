f92cb6d44d4febef82d55a2040b3b274d7071070
Mohammad Behnam <mohammadbehnam.dev@gmail.com>
2024-09-22 11:32:59 +0330

    add task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -14,6 +15,7 @@ import { ProjectsModule } from './projects/projects.module';
       synchronize: true,
     }),
     ProjectsModule,
+    TasksModule,
   ],
 })
 export class AppModule {}
