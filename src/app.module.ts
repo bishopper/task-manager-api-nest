7abfe2394a29e043e631d09d2fe29228a927b709
Mohammad Behnam <mohammadbehnam.dev@gmail.com>
2024-09-22 10:27:56 +0330

    add some chane in project module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectsModule } from './projects/projects.module';
-
 @Module({
-  imports: [ProjectsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'M1379m1379',
+      database: 'task_manager_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ProjectsModule,
+  ],
 })
 export class AppModule {}
--- src/projects/entities/project.entity.ts
+++ src/projects/entities/project.entity.ts
@@ -1,1 +1,13 @@
-export class Project {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'projects' })
+export class Project {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  //    @Column()
+  //   status:
+}
