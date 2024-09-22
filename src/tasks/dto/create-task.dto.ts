8f0137ba908b3facfc36b911f29d5c9d3cba0aec
Mohammad Behnam <mohammadbehnam.dev@gmail.com>
2024-09-22 12:30:35 +0330

    add swigger to tasks

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,8 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { TaskStatus } from '../entities/task.entity';
 
 export class CreateTaskDto {
+  @ApiProperty({ description: 'The title of the task' })
   title: string;
+
+  @ApiProperty({ description: 'The description of the task', required: false })
   description?: string;
+
+  @ApiProperty({ enum: TaskStatus, description: 'The status of the task' })
   status: TaskStatus;
+
+  @ApiProperty({
+    description: 'The ID of the project that the task belongs to',
+  })
   projectId: number;
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,33 +1,62 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { TasksService } from './tasks.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { Task } from './entities/task.entity'; // برای استفاده در پاسخ Swagger
+import { TasksService } from './tasks.service';
 
+@ApiTags('tasks') // تعریف یک تگ برای همه متدهای کنترلر
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new task' })
+  @ApiResponse({
+    status: 201,
+    description: 'The task has been created.',
+    type: Task,
+  })
   create(@Body() createTaskDto: CreateTaskDto) {
     return this.tasksService.create(createTaskDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Retrieve all tasks' })
+  @ApiResponse({ status: 200, description: 'List of tasks', type: [Task] })
   findAll() {
     return this.tasksService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Retrieve a specific task' })
+  @ApiResponse({ status: 200, description: 'Task found.', type: Task })
   findOne(@Param('id') id: string) {
     return this.tasksService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a task' })
+  @ApiResponse({
+    status: 200,
+    description: 'The task has been updated.',
+    type: Task,
+  })
   update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
     return this.tasksService.update(+id, updateTaskDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a task' })
+  @ApiResponse({ status: 204, description: 'The task has been deleted.' })
   remove(@Param('id') id: string) {
     return this.tasksService.remove(+id);
   }
